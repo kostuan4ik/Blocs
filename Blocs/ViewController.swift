f8585a9c1bc3fb636d223ce8f16c0ecac2716111
kostuan4ik <oxxymirom@gmail.com>
2024-06-01 21:35:28 -0700

    Initial Commit

---
+++ Blocs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blocs
+//
+//  Created by zxz on 6/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
